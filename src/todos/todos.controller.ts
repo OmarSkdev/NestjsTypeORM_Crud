95e67a5c0f9a5222e3a85a18a6a2c97eb9418354
Omar <omar.sk80@gmail.com>
2024-10-03 22:31:10 -0300

    first commit

---
+++ src/todos/todos.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Post, Get, Put, Param, Delete } from "@nestjs/common";
+import { TodosService } from "./todos.service";
+import { CreateTodoDto } from "./dtos/create-todo.dto";
+
+@Controller('todos')
+export class TodosController {
+    constructor(private readonly todosService: TodosService){}
+
+    @Post()
+    create(@Body() dto:CreateTodoDto){
+        return this.todosService.create(dto)
+    }
+
+    @Get()
+    findMany() {
+        return this.todosService.findMany();
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() dto: CreateTodoDto) {
+        return this.todosService.update(id, dto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: number) {
+        return this.todosService.delete(id);
+    }
+}
